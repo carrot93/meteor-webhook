6a53acadf4092d3fd452175c79323153527c6c11
Ben Wiley <ben.wiley@newspring.cc>
2015-01-31 17:11:21 -0500

    Version bump

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'bjwiley2:webhook',
   summary: 'Quick and easy WebHook functionality for your Meteor project.',
-  version: '0.0.11',
+  version: '0.0.12',
   git: 'https://github.com/NewSpring/webhook.git'
 });
 
